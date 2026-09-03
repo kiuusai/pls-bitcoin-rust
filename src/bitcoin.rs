698e2a71cabc98de10323d28cc0e2ab791744d42
kiuusai <kiuusai@protonmail.com>
2026-09-03 05:27:04 -0300

    Adding utils crate

--- src/bitcoin.rs
+++ src/bitcoin.rs
@@ -1,5 +1,7 @@
 use std::vec;
 
+use crate::utils::*;
+
 use bitcoin::script::Builder;
 use bitcoin::secp256k1::PublicKey;
 use bitcoin::taproot::{TapTree, TaprootBuilder};
@@ -14,42 +16,6 @@ pub struct MultisigScript {
 pub struct Multisig {
     address: Address,
     multisig_scripts: Vec<MultisigScript>,
-}
-
-fn combine<T: Clone>(items: &[T], size: usize) -> Vec<Vec<T>> {
-    if size == 0 {
-        return Vec::new();
-    }
-
-    let mut result = Vec::new();
-    let mut current = Vec::with_capacity(size);
-
-    generate_combinations(items, size, 0, &mut current, &mut result);
-
-    result
-}
-
-fn generate_combinations<T: Clone>(
-    items: &[T],
-    size: usize,
-    start: usize,
-    current: &mut Vec<T>,
-    result: &mut Vec<Vec<T>>,
-) {
-    if current.len() == size {
-        result.push(current.clone());
-        return;
-    }
-
-    for index in start..items.len() {
-        current.push(items[index].clone());
-
-        // Only consider later items, preventing reordered duplicates.
-        generate_combinations(items, size, index + 1, current, result);
-
-        // Undo the choice before trying the next item.
-        current.pop();
-    }
 }
 
 impl Multisig {
