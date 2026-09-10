9c208d84e3894d8d2e00d1518a6f611aabfc8560
kiuusai <kiuusai@protonmail.com>
2026-09-09 21:32:00 -0300

    Adding test to start_tx_spend

--- tests/multisig.rs
+++ tests/multisig.rs
@@ -1,16 +1,41 @@
 mod multisig_integration_tests {
-    use std::{assert_eq, println, vec};
+    use std::{assert_eq, env, println, vec};
 
-    use bitcoin::key::rand::thread_rng;
-    use bitcoincore_rpc::{Auth, Client, RpcApi};
-    use pls_bitcoin_lib::multisig::{Multisig, MultisigOptions};
+    use pls_bitcoin_lib::multisig::{Multisig, MultisigOptions, Utxo};
     use pls_bitcoin_lib::utils;
 
+    use bitcoin::key::rand::thread_rng;
     use bitcoin::key::Keypair;
     use bitcoin::script::Builder;
-    use bitcoin::secp256k1::{PublicKey, Secp256k1, SecretKey};
-    use bitcoin::taproot::{TapTree, TaprootBuilder};
-    use bitcoin::{opcodes, Address, Amount, Network, ScriptBuf, XOnlyPublicKey};
+    use bitcoin::secp256k1::{Message, PublicKey, Secp256k1, SecretKey};
+    use bitcoin::sighash::{Prevouts, SighashCache};
+    use bitcoin::taproot::{self, LeafVersion, TapTree, TaprootBuilder};
+    use bitcoin::{
+        opcodes, Address, Amount, Network, OutPoint, PrivateKey, ScriptBuf, TapLeafHash,
+        TapSighashType, TxOut, Witness, XOnlyPublicKey,
+    };
+    use bitcoincore_rpc::json::EstimateMode;
+    use bitcoincore_rpc::{Auth, Client, RpcApi};
+    use dotenv::dotenv;
+
+    #[derive(Debug, Clone)]
+    struct TestConfig {
+        node_url: String,
+        user: String,
+        pass: String,
+    }
+
+    fn config() -> TestConfig {
+        // It loads the dotenv and ignore errors if file doesn't exists
+        let _ = dotenv();
+
+        return TestConfig {
+            // Use nigiri to make it works instantly
+            node_url: env::var("RPC_NODE_URL").unwrap_or(String::from("http://0.0.0.0:18443")),
+            user: env::var("RPC_USER").unwrap_or(String::from("admin1")),
+            pass: env::var("RPC_PASSWORD").unwrap_or(String::from("123")),
+        };
+    }
 
     #[test]
     fn it_verifies_multisig_creation() {
@@ -145,5 +170,223 @@ mod multisig_integration_tests {
         assert_eq!(address, multisig.address());
 
         println!("Bitcoin address: {}", multisig.address().to_string());
+    }
+
+    #[test]
+    fn it_spends_multisig_values() {
+        let config = config();
+
+        let secp = Secp256k1::new();
+        let rng = &mut thread_rng();
+
+        let client =
+            Client::new(&config.node_url, Auth::UserPass(config.user, config.pass)).unwrap();
+
+        let mut parts_keypairs: Vec<Keypair> = Vec::new();
+
+        for _ in 0..2 {
+            let secret_key = SecretKey::new(rng);
+            let keypair = Keypair::from_secret_key(&secp, &secret_key);
+
+            parts_keypairs.push(keypair);
+        }
+
+        let secret_key = SecretKey::new(rng);
+        let keypair = Keypair::from_secret_key(&secp, &secret_key);
+
+        let arbitrators: Vec<Keypair> = vec![keypair];
+
+        let secret_key = SecretKey::new(rng);
+        let internal_pubkey = Keypair::from_secret_key(&secp, &secret_key).public_key();
+
+        let network = Network::Regtest;
+        let quorum = 1;
+
+        let multisig = Multisig::new(MultisigOptions {
+            parts: parts_keypairs
+                .iter()
+                .map(|part| part.public_key())
+                .collect(),
+            quorum,
+            arbitrators: arbitrators
+                .iter()
+                .map(|arbitrator| arbitrator.public_key())
+                .collect(),
+            internal_pubkey,
+            network,
+        });
+
+        // Needed to generate spendable amounts
+        if client.get_block_count().unwrap() < 100 {
+            let ghost_address = {
+                let secret_key = PrivateKey::generate(network);
+                let public_key = secret_key.public_key(&secp);
+
+                Address::p2pkh(public_key.pubkey_hash(), network)
+            };
+
+            client.generate_to_address(101, &ghost_address).unwrap();
+        }
+
+        let rpc_address = client
+            .get_new_address(None, None)
+            .unwrap()
+            .require_network(network)
+            .unwrap();
+
+        client.generate_to_address(1, &rpc_address).unwrap();
+
+        let txid = client
+            .send_to_address(
+                &multisig.address(),
+                client.get_balance(Some(0), None).unwrap(),
+                Some("send to multisig"),
+                None,
+                Some(true),
+                None,
+                None,
+                Some(EstimateMode::Economical),
+            )
+            .unwrap();
+
+        let tx = client.get_raw_transaction(&txid, None).unwrap();
+
+        let (output_index, output) = tx
+            .output
+            .iter()
+            .enumerate()
+            .find(|(_, output)| {
+                let address = Address::from_script(&output.script_pubkey, network).unwrap();
+
+                return address == multisig.address();
+            })
+            .unwrap();
+
+        println!(
+            "sent {} btc to {} address",
+            output.value,
+            multisig.address()
+        );
+
+        let output_address = Address::from_script(&output.script_pubkey, network).unwrap();
+
+        assert_eq!(output_address, multisig.address());
+
+        let redeem_script = multisig
+            .scripts()
+            .into_iter()
+            .find(|script| {
+                let found_keys: Vec<PublicKey> = script
+                    .combination
+                    .clone()
+                    .into_iter()
+                    .filter(|key| {
+                        parts_keypairs
+                            .iter()
+                            .find(|part| part.public_key().eq(key))
+                            .is_some()
+                    })
+                    .collect();
+
+                return found_keys.len() == parts_keypairs.len();
+            })
+            .unwrap();
+
+        let utxos = vec![Utxo {
+            value: output.value,
+            outpoint: OutPoint::new(txid, output_index as u32),
+        }];
+
+        let redeemer_private_key =
+            bitcoin::PrivateKey::new(parts_keypairs[0].secret_key(), network);
+        let redeemer = redeemer_private_key.public_key(&secp);
+
+        let redeemer_address = Address::p2pkh(redeemer.pubkey_hash(), network);
+
+        let outs = vec![TxOut {
+            value: output.value - Amount::from_sat(200),
+            script_pubkey: redeemer_address.script_pubkey(),
+        }];
+
+        let mut psbt = multisig.start_tx_spending(redeem_script.leaf.clone(), utxos.clone(), outs.clone());
+
+        let unsigned_tx = psbt.unsigned_tx.clone();
+        let mut sighash_cache = SighashCache::new(unsigned_tx);
+
+        let leaf_hash = TapLeafHash::from_script(
+            redeem_script.leaf.clone().as_script(),
+            LeafVersion::TapScript,
+        );
+
+        let prevouts: Vec<TxOut> = utxos
+            .clone()
+            .iter()
+            .map(|utxo| TxOut { value: utxo.value, script_pubkey: multisig.address().script_pubkey() })
+            .collect();
+
+        for i in 0..psbt.inputs.len() {
+            let sighash = sighash_cache
+                .taproot_script_spend_signature_hash(
+                    i,
+                    &Prevouts::All(&prevouts),
+                    leaf_hash,
+                    TapSighashType::Default,
+                )
+                .unwrap();
+            let message = Message::from(sighash);
+
+            for keypair in parts_keypairs.iter() {
+                let signature = secp.sign_schnorr(&message, keypair);
+
+                let final_signature = taproot::Signature {
+                    signature,
+                    sighash_type: TapSighashType::Default,
+                };
+
+                let (xonly_key, _) = keypair.x_only_public_key();
+
+                psbt.inputs[i]
+                    .tap_script_sigs
+                    .insert((xonly_key, leaf_hash), final_signature);
+            }
+        }
+
+        let is_completed = parts_keypairs.iter().all(|keypair| {
+            let (xonly, _) = keypair.x_only_public_key();
+
+            psbt.inputs
+                .clone()
+                .iter()
+                .all(|input| input.tap_script_sigs.contains_key(&(xonly, leaf_hash)))
+        });
+
+        assert!(is_completed);
+
+        psbt.inputs.iter_mut().for_each(|input| {
+            let mut witness = Witness::new();
+
+            for keypair in parts_keypairs.iter().rev() {
+                let (xonly, _) = keypair.x_only_public_key();
+                let sig = input
+                    .tap_script_sigs
+                    .get(&(xonly, leaf_hash))
+                    .unwrap()
+                    .clone();
+                witness.push(sig.to_vec());
+            }
+
+            let control_block = input.tap_scripts.iter().next().unwrap().0;
+
+            witness.push(redeem_script.leaf.clone().to_bytes());
+            witness.push(control_block.serialize());
+
+            input.final_script_witness = Some(witness);
+            input.tap_script_sigs.clear();
+            input.tap_scripts.clear();
+        });
+
+        let tx = psbt.extract_tx().unwrap();
+
+        client.send_raw_transaction(&tx).unwrap();
     }
 }
